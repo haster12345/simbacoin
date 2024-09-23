8d631df9fab509fdc36eb17c206d9da51c81f3fd
haster12345 <tarunmankani25@gmail.com>
2024-09-23 18:31:05 +0100

    implemented sha256

---
+++ logical.py
@@ -0,0 +1,22 @@
+class Logical:
+    @staticmethod
+    def _right_rot(x: int, n: int) -> int:
+        return ((x >> n) | (x << (32 - n))) & 0xFFFFFFFF
+
+    def Ch(self, x: int, y: int, z: int) -> int:
+        return (x & y) ^ (~x & z)
+
+    def Mag(self, x: int, y: int, z: int) -> int:
+        return (x & y) ^ (x & z) ^ (y & z)
+
+    def sigma0(self, x: int) -> int:
+        return self._right_rot(x, 2) ^ self._right_rot(x, 13) ^ self._right_rot(x, 22)
+
+    def sigma1(self, x: int) -> int:
+        return self._right_rot(x, 6) ^ self._right_rot(x, 11) ^ self._right_rot(x, 25)
+
+    def sig0(self, x: int) -> int:
+        return self._right_rot(x, 7) ^ self._right_rot(x, 18) ^ (x >> 3)
+
+    def sig1(self, x: int) -> int:
+        return self._right_rot(x, 17) ^ self._right_rot(x, 19) ^ (x >> 10)
--- main.py
+++ main.py
@@ -1,54 +1,38 @@
-class Logical:
-    def Ch(self, x, y, z):
-        return bin((x & y) ^ (~x & z))
-
-    def Mag(self, x, y, z):
-        return bin((x & y) ^ (x & z) ^ (y & z))
-
-    def sigma0(self, x):
-        return bin((x >> 2) ^ (x >> 13) ^ (x >> 22))
-
-    def sigma1(self, x):
-        return bin((x >> 6) ^ (x >> 11) ^ (x >> 25))
-
-    def sig0(self, x):
-        return bin((x >> 7) ^ (x >> 18) ^ (x << 3))
-
-    def sig1(self, x):
-        return bin((x >> 17) ^ (x >> 19) ^ (x << 10))
-
-
-def int_to_big_endian(number: int) -> str:
-    """
-    converts int to binary and then outputs as big endian binary
-    :param number: int
-    :return: a string with len 64, representing a binary number in big endian
-    """
-    bin_int = "{0:b}".format(number * 8)
-    zeros = 64 - len(bin_int)
-    return '0' * zeros + bin_int
+from logical import Logical
 
 
 def pre_processing(msg: str) -> list[int]:
     msg_ascii = msg.encode(encoding='ascii', errors='ignore')
 
     zero_bits = (448 - (len(msg_ascii) * 8) - 1) % 512
-    bin_msg = bin(int.from_bytes(msg_ascii, 'big'))
-    padded_msg = bin_msg + '1' + ('0' * zero_bits) + int_to_big_endian(len(msg_ascii))
-    n = 512
-    blocks = [int(padded_msg[i:i + n], 2) for i in range(0, len(padded_msg), n)]
+    bin_msg = int.from_bytes(msg_ascii, 'big')
+    padded_msg = ((((bin_msg << 1) + 1) << zero_bits) << 64) + len(msg_ascii) * 8
+
+    blocks = []
+    i = 0
+    while i <= padded_msg.bit_length():
+        n1 = padded_msg >> i & (2 ** 512 - 1)
+        i += 512
+        blocks.append(n1)
+
     return blocks
 
 
-def expanded_msg_blocks(blocks, index_i, index_j):
-    if index_i <= 15:
-        return blocks[index_i][32*index_j: 32*index_j + 32]
+def expanded_msg_blocks(block, index_j):
+    if index_j <= 15:
+        return block >> (index_j * 32) & (2 ** 32 - 1)
     else:
-        return ((int(Logical().sig1(expanded_msg_blocks(blocks, index_i, index_j - 2)), 2) +
-                int(Logical().sig0(expanded_msg_blocks(blocks, index_i, index_j - 15))), 2)
-                + expanded_msg_blocks(blocks, index_i, index_j - 16)) % 2 ** 32
+        return ((Logical().sig1(expanded_msg_blocks(block, index_j - 2)) +
+                 Logical().sig0(expanded_msg_blocks(block, index_j - 15)))
+                + expanded_msg_blocks(block, index_j - 16)) % 2 ** 32
+
 
 def main(text):
+    """
+    j = 0;
+        T1  = 0xbdbb52b0
+            = 3183170224 = 0x5be0cd19 ( h ) + 0x1f5055e7 (sigma1(e) + Ch(e, f, g)) + 24 ( W ) + 0x428a2f98 (K)
+    """
     blocks = pre_processing(text)
 
     hash_values = [[0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]]
@@ -63,25 +47,28 @@ def main(text):
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
     ]
 
-    for i in range(1, len(blocks)):
+    lg = Logical()
+
+    for i in range(1, len(blocks) + 1):
         a, b, c, d, e, f, g, h = hash_values[i - 1]
 
         for j in range(63 + 1):
-            W = expanded_msg_blocks(blocks, i, j)
-            T1 = (h + int(Logical().sigma1(e), 2) + int(Logical().Ch(e, f, g), 2) + constant_words[j] + W) % 2 ** 32
-            T2 = (int(Logical().sigma0(a), 2) + int(Logical().Mag(a, b, c), 2)) % 2 ** 32
+            W = expanded_msg_blocks(blocks[i - 1], j)
+            T1 = (h + lg.sigma1(e) + lg.Ch(e, f, g) + constant_words[j] + W) % (2 ** 32)
+            T2 = (lg.sigma0(a) + lg.Mag(a, b, c)) % (2 ** 32)
             h = g
             g = f
             f = e
-            e = d + T1
+            e = d + T1 % (2 ** 32)
             d = c
             c = b
             b = a
-            a = T1 + T2
+            a = T1 + T2 % (2 ** 32)
 
         hash_values_i = [0] * 8
+
         for j, var in enumerate([a, b, c, d, e, f, g, h]):
-            hash_values_i[j] = var + hash_values[i-1][j]
+            hash_values_i[j] = var + hash_values[i - 1][j]
         hash_values.append(hash_values_i)
 
     return [hex(i) for i in hash_values[-1]]
