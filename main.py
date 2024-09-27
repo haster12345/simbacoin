a812e371dbd573f0b7c6946f22e31b04056ba575
haster12345 <tarunmankani25@gmail.com>
2024-09-27 18:56:04 +0100

    implementing ECDSA

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 from functools import lru_cache
 from logical import Logical
+from decimal import Decimal
 
 
 def pre_processing(msg: str) -> list[int]:
@@ -84,19 +85,37 @@ class EC:
         self.p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
         self.a = 0x0
         self.b = 0x7
-        self.G = 0x0479BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+        self.G_x = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+        self.G_y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
         self.n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
         self.h = 1
         self.x = x
         self.y = y
 
+    @property
+    def xy(self):
+        return hex(self.x), hex(self.y)
 
+
+@lru_cache
 def add_points(P: EC, Q: EC) -> EC:
-    if P != Q:
-        lam = (Q.y - P.y) / (Q.x - Q.x)
+    assert isinstance(P, EC), f"P must be an Elliptic Curve with type {EC}, got {P} instead"
+    assert isinstance(Q, EC), f"Q must be an Elliptic Curve with type {EC}, got {Q} instead"
+    assert P or Q, f"Can not add two null points"
+
+    if not P:
+        return Q
+    if not Q:
+        return P
+
+    if P.x != Q.x and P.y != Q.y:
+        lam = (Q.y - P.y) / (Q.x - P.x)
+    elif P.x == Q.x:
+        return EC()
     else:
-        lam = 3 * P.x ** 2 + P.a
-    x_r = lam ** 2 - P.x - Q.x
+        lam = 3 * (P.x << 1) + P.a
+
+    x_r = (lam << 1) - P.x - Q.x
     y_r = lam * (P.x - x_r) - P.y
     return EC(x_r, y_r)
 
@@ -104,21 +123,22 @@ def add_points(P: EC, Q: EC) -> EC:
 def mult_points(P: EC, d):
     if d == 0:
         return 0
-    elif d == 0:
+    elif d == 1:
         return P
-    elif d % 2 == 1:
-        return add_points(P, mult_points(P, mult_points(P, d - 1)))
+    elif int(d % 2) == 1:
+        return add_points(P, mult_points(P, d - 1))
     else:
-        return mult_points(add_points(P, P), d / 2)
+        return mult_points(add_points(P, P), int(d / 2))
 
 
 def ecdsa(message, sk):
     """
     curve = secp256k1 -> y^2 = x^3 + 7
     """
-    z = message
+    concat_hex = ''.join(h[2:] for h in [hex(i) for i in message])
+    z = int(concat_hex, 16)
     k = random.randint(1, EC(0, 0).n - 1)
-    curve_points = mult_points(EC(), k)
+    curve_points = mult_points(EC(EC().G_x, EC().G_y), k)
     r = curve_points.x % EC().n
     s = k ** - 1 * (z + r * sk) % EC().n
     return r, s
@@ -140,5 +160,5 @@ def main():
 
 if __name__ == '__main__':
     hash_vals = sha256(text="A pays B 100")
-
     print([hex(i) for i in hash_vals])
+    print(main())
